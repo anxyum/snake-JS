ef2c1cdb5208969b135180e95e69739e78651561
anxyum <115811968+anxyum@users.noreply.github.com>
2024-11-26 15:57:42 +0100

    VIVE LE SAUCISSON

--- saucisson.js
+++ saucisson.js
@@ -5,6 +5,7 @@ let apple = { x: 5, y: 5 };
 let direction = { x: 0, y: 0 };
 let speed = 200;
 let game = false;
+let keys_pressed = [];
 
 function createGrid() {
   for (let i = 0; i < gridSize * gridSize; i++) {
@@ -12,20 +13,22 @@ function createGrid() {
     gameContainer.appendChild(cell);
   }
 }
+
 function draw() {
   gameContainer.childNodes.forEach((cell) => {
     cell.className = "";
   });
   gameContainer.childNodes[apple.y * 20 + apple.x].classList.add("apple");
-  for (let i = 0; i < snake.length-1; i++) {
-    gameContainer.childNodes[(snake[i].y * 20 + snake[i].x)].classList.add(
+  for (let i = 0; i < snake.length - 1; i++) {
+    gameContainer.childNodes[snake[i].y * 20 + snake[i].x].classList.add(
       "snake"
     );
   }
-  gameContainer.childNodes[(snake[snake.length - 1].y * 20 + snake[snake.length - 1].x)].classList.add(
-    "snake-head"
-  );
+  gameContainer.childNodes[
+    snake[snake.length - 1].y * 20 + snake[snake.length - 1].x
+  ].classList.add("snake-head");
 }
+
 function gameOver() {
   alert(`Game Over\nscore: ${snake.length - 1}`);
   snake = [{ x: 10, y: 10 }];
@@ -34,10 +37,31 @@ function gameOver() {
   game = false;
   speed = 200;
 }
+
 function update() {
   if (!game) {
-    return
+    return;
   }
+
+  if (keys_pressed.length > 0) {
+    direction = keys_pressed[0];
+    keys_pressed.shift();
+    switch (direction) {
+      case "ArrowUp":
+        direction = { x: 0, y: -1 };
+        break;
+      case "ArrowDown":
+        direction = { x: 0, y: 1 };
+        break;
+      case "ArrowLeft":
+        direction = { x: -1, y: 0 };
+        break;
+      case "ArrowRight":
+        direction = { x: 1, y: 0 };
+        break;
+    }
+  }
+
   const head = snake[snake.length - 1];
   const newHead = { x: head.x + direction.x, y: head.y + direction.y };
   for (let i = 0; i < snake.length; i++) {
@@ -46,6 +70,7 @@ function update() {
       return;
     }
   }
+
   if (
     gridSize - 1 < newHead.x ||
     newHead.x < 0 ||
@@ -55,6 +80,7 @@ function update() {
     gameOver();
     return;
   }
+  
   snake.push(newHead);
   if (newHead.x !== apple.x || newHead.y !== apple.y) {
     snake.shift();
@@ -64,30 +90,20 @@ function update() {
     apple.y = Math.floor(Math.random() * gridSize);
   }
 }
+
 function gameLoop() {
   update();
   draw();
   setTimeout(gameLoop, speed);
 }
+
 function handleKey(event) {
   if (!game) {
     game = true;
   }
-  switch (event.key) {
-    case "ArrowUp":
-      if (direction.y === 0) direction = { x: 0, y: -1 };
-      break;
-    case "ArrowDown":
-      if (direction.y === 0) direction = { x: 0, y: 1 };
-      break;
-    case "ArrowLeft":
-      if (direction.x === 0) direction = { x: -1, y: 0 };
-      break;
-    case "ArrowRight":
-      if (direction.x === 0) direction = { x: 1, y: 0 };
-      break;
-  }
+  keys_pressed.push(event.key);
 }
+
 document.addEventListener("keydown", handleKey);
 createGrid();
 gameLoop();
